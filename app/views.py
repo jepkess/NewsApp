27d8a93cdc139f5539c400ff83fb1b31bd7fbcdf
jepkess <biivincent1997@gmail.com>
2021-10-29 20:41:59 +0300

    creating the configuration class

--- app/views.py
+++ app/views.py
@@ -7,5 +7,17 @@ def index():
 
     '''
     View root page function that returns the index page and its data
+
     '''
-    return render_template('index.html')
+    message ="Welcome to News-App"
+    return render_template('index.html',message=message)
+
+@app.route('/articles/<source>')
+def articles():
+    """
+     View articles for a specific source page function that returns the article details page and its data.
+    """
+    return render_template('article.html')
+
+
+
