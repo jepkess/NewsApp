079166dd3673ff9551b6e80d1e02e241237fd3a5
jepkess <biivincent1997@gmail.com>
2021-10-30 17:13:25 +0300

    create the class source on the article.py file

--- app/models/article.py
+++ app/models/article.py
@@ -10,3 +10,14 @@ class Article:
         self.url = url
         self.image = image
         self.publishedAt = publishedAt
+
+
+class Source:
+    def __init__(self,id,name,description,url,category,country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category  
+        self.country = country
+
