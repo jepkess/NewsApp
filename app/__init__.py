e55bb24386100298095970a7b3076ae90f4237fb
jepkess <biivincent1997@gmail.com>
2021-10-30 11:31:46 +0300

    create a request file and add the api key

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from .config import DevConfig
 #initializing the application
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True) #passing in the instance which allow us to connect to the instance folder when the app instance is created.
 # Setting up configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 from app import views
---
+++ app/requests.py
@@ -0,0 +1,4 @@
+from app import app
+
+#Getting the api key
+api_key = app.config('8fdfde29d80c4409b7832f1703584947')
