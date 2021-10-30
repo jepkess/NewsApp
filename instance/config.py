2e2b6b7bf0e6b75bf4dca2977763c12d4b402210
jepkess <biivincent1997@gmail.com>
2021-10-30 17:15:20 +0300

    configure the apikey

--- instance/config.py
+++ instance/config.py
@@ -1,1 +1,3 @@
-News_Api_key='8fdfde29d80c4409b7832f1703584947'
+API_KEY='530e4717b7f440a7a60851bca8f06680'
+# NEWS_SOURCES_API_BASE_URL=  'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+
