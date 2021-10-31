90e4f0e097982e32e6e3da7c295e1581c74769f8
jepkess <biivincent1997@gmail.com>
2021-10-31 16:48:04 +0300

    change the image and title arguments

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ class Config:
     General configuration parent class
     '''
     API_KEY='530e4717b7f440a7a60851bca8f06680'
-    NEWS_ARTICLES_API_BASE_URL='https://newsapi.org/v2/{}?q=tesla&from=2021-09-30&sortBy=publishedAt&apiKey={}'
+    SPECIFIC_ARTICLES_BASE_URL='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     NEWS_SOURCES_API_BASE_URL='https://newsapi.org/v2/top-headlines/{}?apiKey={}'
 
 
--- app/request.py
+++ app/request.py
@@ -45,10 +45,10 @@ def process_results(news_list):
     for news_item in news_list:
       
         author = news_item.get('author')
-        title = news_item.get('original_title')
+        title = news_item.get('title')
         description = news_item.get('description')
         url = news_item.get('url')
-        image = news_item.get('image')
+        image = news_item.get('urlToImage')
         publishedAt = news_item.get('publishedAt')
 
         if author:
