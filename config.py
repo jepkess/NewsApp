569740e853bec7acca31293781a4c3d974023cdb
jepkess <biivincent1997@gmail.com>
2021-10-31 23:56:50 +0300

    fix back config.py file

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SPECIFIC_ARTICLES_BASE_URL='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     NEWS_SOURCES_API_BASE_URL='https://newsapi.org/v2/top-headlines/{}?apiKey={}'
     # API_KEY='530e4717b7f440a7a60851bca8f06680'
-    MOVIE_API_KEY = os.environ.get('API_KEY')
+    NEWS_API_KEY = os.environ.get('API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     
 
