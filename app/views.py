b210a1d8c1e9f6f66c3f986b85edc43ca0ffb8de
jepkess <biivincent1997@gmail.com>
2021-10-31 16:49:24 +0300

    display the source on the view function

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from flask import render_template
 from app import app
-from .request import get_news,get_source
+from .request import get_articles,get_source
 
 # Views
+#displaying the source on the homepage
 @app.route('/')
 def index():
 
@@ -10,33 +11,33 @@ def index():
     View root page function that returns the index page and its data
 
     '''
-    
+    response=get_source('sources')
     title = "news"
-    return render_template('index.html',title=title)
+    return render_template('index.html',title=title,sources=response)
 
-@app.route('/articles')
-def articles():
+@app.route('/articles/<source>',methods=['GET']) # get method to get data from the api
+def articles(source):
 
     '''
     View root page function that returns the index page and its data
 
     '''
-    technology_news=get_news('everything')
+    articles=get_articles(source)
    
     title = " HOME-Welcome to the best News online website"
-    return render_template('articles.html',title=title,technology_news=technology_news)
+    return render_template('articles.html',title=title,articles=articles,source=source)
 
-@app.route('/sources')
-def sources():
+# @app.route('/')
+# def sources():
 
-    '''
-    View root page function that returns the index page and its data
+#     '''
+#     View root page function that returns the index page and its data
 
-    '''
+#     '''
   
-    news_sources=get_source('sources')
-    title = " HOME-Welcome to the best News online website"
-    return render_template('sources.html',title=title,news_sources=news_sources)
+#     news_sources=get_source('sources')
+#     title = " HOME-Welcome to the best News online website"
+#     return render_template('sources.html',title=title,news_sources=news_sources)
 
 
 
