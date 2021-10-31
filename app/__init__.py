acb8de35f5b77d949ab9a8a966f7012f97606454
jepkess <biivincent1997@gmail.com>
2021-10-31 19:18:19 +0300

    initializing he application

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,35 @@
+# from flask import Flask
+# from config import DevConfig
+# from flask_bootstrap import Bootstrap
+# from config import config_options
+# #initializing the application
+# app = Flask(__name__,instance_relative_config = True) #passing in the instance which allow us to connect to the instance folder when the app instance is created.
+# # Setting up configuration
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile('config.py')
+
+# #initializing the flask extensions
+# bootstrap = Bootstrap(app)
+
+
+
+# from app import views
 from flask import Flask
-from .config import DevConfig
 from flask_bootstrap import Bootstrap
-#initializing the application
-app = Flask(__name__,instance_relative_config = True) #passing in the instance which allow us to connect to the instance folder when the app instance is created.
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+from config import config_options
 
-#initializing the flask extensions
-bootstrap = Bootstrap(app)
+bootstrap = Bootstrap()
 
+def create_app(config_name):
 
+    app = Flask(__name__)
 
-from app import views
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+     # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
